a0b2173f385b7a38becbbb8726a5c1fbd04430ca
SadieF <sadiefreeman@gmail.com>
2018-08-21 23:27:33 -0700

    Add GoDoc Comments

--- game/game.go
+++ game/game.go
@@ -1,23 +1,28 @@
 package game
 
+// Pos determines the position on the grid using X & Y axis
 type Pos struct {
 	X int
 	Y int
 }
 
+// Size determines the size of the grid
 type Size struct {
 	Height int
 	Width  int
 }
+
+// Player defines the struct with player information
 type Player struct {
 	Pos      Pos
 	Name     string
-	Id       string
-	Cooldown int64
+	ID       string
+	Cooldown int
 	Money    uint
 }
 
-type GameState struct {
+//State is the struct which represents the game state
+type State struct {
 	Players    map[string]Player
 	Start      int64
 	Treasures  []Pos
